8bf7a861ab25bd077df11b4edfed9f95212e5aee
Giada <tonni.giada.210802@gmail.com>
2026-01-26 16:24:06 +0100

    classi tratta e storico percorsi fatte

---
+++ src/main/java/giada_tonni/entities/StoricoPercorsi.java
@@ -0,0 +1,72 @@
+package giada_tonni.entities;
+
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "percorsi")
+public class StoricoPercorsi {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "storico_percorso_id")
+    private UUID storicoPercorsoId;
+
+    @Column(name = "tempo_percorso_effettivo")
+    private long tempoEffettivo;
+
+    @ManyToOne
+    private Tratta tratta;
+
+    @ManyToOne
+    private Mezzo mezzo;
+
+    public StoricoPercorsi() {
+    }
+
+    public StoricoPercorsi(long tempoEffettivo, Tratta tratta, Mezzo mezzo) {
+        this.tempoEffettivo = tempoEffettivo;
+        this.tratta = tratta;
+        this.mezzo = mezzo;
+    }
+
+    public UUID getStoricoPercorsoId() {
+        return storicoPercorsoId;
+    }
+
+    public long getTempoEffettivo() {
+        return tempoEffettivo;
+    }
+
+    public void setTempoEffettivo(long tempoEffettivo) {
+        this.tempoEffettivo = tempoEffettivo;
+    }
+
+    public Tratta getTratta() {
+        return tratta;
+    }
+
+    public void setTratta(Tratta tratta) {
+        this.tratta = tratta;
+    }
+
+    public Mezzo getMezzo() {
+        return mezzo;
+    }
+
+    public void setMezzo(Mezzo mezzo) {
+        this.mezzo = mezzo;
+    }
+
+    @Override
+    public String toString() {
+        return "StoricoPercorsi{" +
+                "storicoPercorsoId=" + storicoPercorsoId +
+                ", tempoEffettivo=" + tempoEffettivo +
+                ", tratta=" + tratta +
+                ", mezzo=" + mezzo +
+                '}';
+    }
+}
---
+++ src/main/java/giada_tonni/entities/Tratta.java
@@ -0,0 +1,72 @@
+package giada_tonni.entities;
+
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "tratte")
+public class Tratta {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "tratta_id")
+    private UUID trattaId;
+
+    @Column(name = "partenza")
+    private String partenza;
+
+    @Column(name = "capolinea")
+    private String capolinea;
+
+    @Column(name = "tempo_percorso_previsto")
+    private long tempoPrevisto;
+
+    public Tratta() {
+    }
+
+    public Tratta(String partenza, String capolinea, long tempoPrevisto) {
+        this.partenza = partenza;
+        this.capolinea = capolinea;
+        this.tempoPrevisto = tempoPrevisto;
+    }
+
+    public UUID getTrattaId() {
+        return trattaId;
+    }
+
+    public String getPartenza() {
+        return partenza;
+    }
+
+    public void setPartenza(String partenza) {
+        this.partenza = partenza;
+    }
+
+    public String getCapolinea() {
+        return capolinea;
+    }
+
+    public void setCapolinea(String capolinea) {
+        this.capolinea = capolinea;
+    }
+
+    public long getTempoPrevisto() {
+        return tempoPrevisto;
+    }
+
+    public void setTempoPrevisto(long tempoPrevisto) {
+        this.tempoPrevisto = tempoPrevisto;
+    }
+
+    @Override
+    public String toString() {
+        return "Tratta{" +
+                "trattaId=" + trattaId +
+                ", partenza='" + partenza + '\'' +
+                ", capolinea='" + capolinea + '\'' +
+                ", tempoPrevisto=" + tempoPrevisto +
+                '}';
+    }
+}
