ee944e9994aaaa645b3639517dab3a6d9801ccda
KeviN-XVII <kevinquartar@gmail.com>
2026-01-27 10:07:19 +0100

    DAO Utenti e Tessera

---
+++ src/main/java/giada_tonni/DAO/TessereDAO.java
@@ -0,0 +1,50 @@
+package giada_tonni.DAO;
+
+import giada_tonni.entities.TesseraUtente;
+import giada_tonni.entities.Utenti;
+import giada_tonni.exceptions.NotFoundException;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+
+import java.util.UUID;
+
+public class TessereDAO {
+   private EntityManager em;
+
+   public TessereDAO(EntityManager em) {
+       this.em = em;
+   }
+
+    //    save
+
+    public void saveTessera (TesseraUtente newTessera) {
+        EntityTransaction transaction = em.getTransaction();
+        transaction.begin();
+
+        em.persist(newTessera);
+
+        transaction.commit();
+
+        System.out.println("L'utente " + newTessera.getNumeroTessera() + " è stata salvata correttamente!");
+    }
+
+// findById
+
+    public TesseraUtente findTesseraById(UUID tesseraId) {
+        TesseraUtente found = em.find(TesseraUtente.class, tesseraId);
+        if (found == null)
+            throw new NotFoundException(tesseraId);
+        return found;
+    }
+
+
+    //    remove
+    public void deleteTesseraById(UUID tesseraId) {
+        TesseraUtente found = em.find(TesseraUtente.class, tesseraId);
+        if (found == null) {
+            throw new NotFoundException(tesseraId);
+        }
+        em.remove(found);
+    }
+
+}
---
+++ src/main/java/giada_tonni/DAO/UtentiDAO.java
@@ -0,0 +1,54 @@
+package giada_tonni.DAO;
+
+import giada_tonni.entities.Utenti;
+import giada_tonni.exceptions.NotFoundException;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+
+import java.util.UUID;
+
+public class UtentiDAO {
+    private EntityManager em;
+
+    public UtentiDAO(EntityManager em) {
+        this.em = em;
+    }
+//    save
+
+public void saveUtente (Utenti newUtente) {
+    EntityTransaction transaction = em.getTransaction();
+    transaction.begin();
+
+    em.persist(newUtente);
+
+    transaction.commit();
+
+    System.out.println("L'utente " + newUtente.getNome() + " è stato salvato correttamente!");
+}
+
+// findById
+
+    public Utenti findUtenteById(UUID utenteId) {
+        Utenti found = em.find(Utenti.class, utenteId);
+        if (found == null)
+            throw new NotFoundException(utenteId);
+        return found;
+    }
+
+
+//    remove
+    public void deleteUtenteById(UUID utenteId) {
+        Utenti found = em.find(Utenti.class, utenteId);
+        if (found == null) {
+            throw new NotFoundException(utenteId);
+        }
+        em.remove(found);
+    }
+
+
+
+
+
+
+
+}
