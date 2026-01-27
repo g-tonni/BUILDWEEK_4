f280a22dfd6cd72271a95b0c96bed136886fbb21
AntonioM. <antoniomeronew@gmail.com>
2026-01-27 12:14:53 +0100

    eliminazione enum stato mezzo

--- src/main/java/giada_tonni/entities/Mezzo.java
+++ src/main/java/giada_tonni/entities/Mezzo.java
@@ -1,6 +1,5 @@
 package giada_tonni.entities;
 
-
 import jakarta.persistence.*;
 import org.hibernate.annotations.UuidGenerator;
 
@@ -9,8 +8,8 @@ import java.util.UUID;
 @Entity
 @Table(name = "mezzi")
 @Inheritance(strategy = InheritanceType.JOINED)
-
 public class Mezzo {
+
     @Id
     @GeneratedValue
     @UuidGenerator
@@ -20,25 +19,16 @@ public class Mezzo {
     @Column(name = "capienza", nullable = false)
     protected long capienza;
 
-
-    @Enumerated(EnumType.STRING)
-    @Column(name = "stato", nullable = false)
-    protected StatoMezzo stato;
-
-
     @Enumerated(EnumType.STRING)
     @Column(name = "tipo", nullable = false)
     protected TipoMezzo tipo;
 
-    //COSTRUTTORE + vuoto
+    // COSTRUTTORE vuoto
+    protected Mezzo() {}
 
-    protected Mezzo() {
-    }
-
-
-    protected Mezzo(long capienza, StatoMezzo stato, TipoMezzo tipo) {
+    // COSTRUTTORE
+    protected Mezzo(long capienza, TipoMezzo tipo) {
         this.capienza = capienza;
-        this.stato = stato;
         this.tipo = tipo;
     }
 
@@ -58,14 +48,6 @@ public class Mezzo {
         this.capienza = capienza;
     }
 
-    public StatoMezzo getStato() {
-        return stato;
-    }
-
-    public void setStato(StatoMezzo stato) {
-        this.stato = stato;
-    }
-
     public TipoMezzo getTipo() {
         return tipo;
     }
@@ -79,7 +61,6 @@ public class Mezzo {
         return "Mezzo{" +
                 "id=" + id +
                 ", capienza=" + capienza +
-                ", stato=" + stato +
                 ", tipo=" + tipo +
                 '}';
     }
