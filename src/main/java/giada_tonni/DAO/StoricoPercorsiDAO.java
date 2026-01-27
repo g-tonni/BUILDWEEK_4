d8c4288da72f3dc753bd1dca7882f45e87970163
Gentjana <hanigentjana@gmail.com>
2026-01-27 10:14:09 +0100

    add_DAO
    storicoPercorsi e puntiVendita DAO

---
+++ src/main/java/giada_tonni/DAO/StoricoPercorsiDAO.java
@@ -0,0 +1,54 @@
+package giada_tonni.DAO;
+
+import giada_tonni.entities.PuntiVendita;
+import giada_tonni.entities.StoricoPercorsi;
+import giada_tonni.exceptions.NotFoundException;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Query;
+
+import java.util.UUID;
+
+public class StoricoPercorsiDAO {
+    private final EntityManager entityManager;
+
+    public StoricoPercorsiDAO(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    public void saveStoricoPercorsi(StoricoPercorsi newStoricoPercorsi) {
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+        entityManager.persist(newStoricoPercorsi);
+        transaction.commit();
+        System.out.println("Percorso: " + newStoricoPercorsi.getStoricoPercorsoId() + " è stato salvato correttamente.");
+    }
+
+    public StoricoPercorsi findStoricoPercorsiById(UUID storicoPercorsiId) {
+        try {
+            return entityManager.createQuery("SELECT s FROM StoricoPercorsi s WHERE s.StoricoPercorsi= :storicoPercorsiId", StoricoPercorsi.class)
+                    .setParameter("storicoPercorsiId", storicoPercorsiId)
+                    .getSingleResult();
+        } catch (NotFoundException exception) {
+            return null;
+        }
+    }
+
+    public void deletesoricoPercorsiById(UUID storicoPercorsiId) {
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+
+        Query query = entityManager.createQuery("DELETE FROM StoricoPercorsi s WHERE s.StoricoPercorsi= :storicoPercorsiId");
+        query.setParameter("storicoPercorsiId", storicoPercorsiId);
+
+        int deleted = query.executeUpdate();
+        transaction.commit();
+        if (deleted > 0) {
+            System.out.println("Ho rimosso: " + deleted + " percorsi.");
+        } else {
+
+            System.out.println("Nessun percorso con id: " + storicoPercorsiId + " trovato.");
+            throw new NotFoundException(storicoPercorsiId);
+        }
+    }
+}
